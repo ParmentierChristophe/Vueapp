d9068711a64333332f179907fa68e5511e2a0876
Crispy <christophe-parmentier@laposte.net>
2017-09-07 13:16:56 +0200

    add style in register page and logo crispy

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+<div id="app">
+  <router-view></router-view>
+</div>
 </template>
 
 <script>
@@ -11,12 +11,17 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: Open Sans, Helvetica, sans-serif;
+  font-weight: 300;
+  height: 100vh;
+  width: 100%;
+  background: #e53935;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #e35d5b, #e53935);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #e35d5b, #e53935); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  font-family: Open Sans, Helvetica, sans-serif;
+  font-weight: 300;
+  overflow-x: hidden;
 }
 </style>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,14 +1,48 @@
 <template>
 <div class="register">
-  <form class="">
-    <input type="text" name="name" placeholder="name" v-model="name" />
-    <input type="email" name="email" placeholder="email address" v-model="email" />
-    <input type="password" name="password" placeholder="password" v-model="password"/>
-    <input type="password" name="password_confirmation" placeholder="password confirmation" v-model="password_confirmation"/>
+  <section class="">
 
-    <button @click.prevent="register">create</button>
-    <p class="">Déjà enregistrer ? <router-link to="/login">Se connecter</router-link></p>
-  </form>
+    <div class="">
+      <div class="center_form">
+        <div class="img">
+
+
+          <img class="logo" src="../assets/crispy.png" alt="">
+        </div>
+        <form class="">
+
+          <div class="form-input">
+            <input class="form-style" type="text" name="name" placeholder="Votre pseudo" v-model.trim="name" />
+          </div>
+
+          <div class="form-input">
+            <input class="form-style" type="email" name="email" placeholder="Votre Email" v-model.trim="email" />
+          </div>
+
+          <div class="form-input">
+            <input class="form-style" type="password" name="password" placeholder="Votre mot de passe" v-model="password" />
+          </div>
+
+          <div class="form-input">
+            <input class="form-style" type="password" name="password_confirmation" placeholder="Confirmer votre mot de passe" v-model="password_confirmation" />
+          </div>
+
+          <div class="form-input">
+
+            <button class="btn" @click.prevent="register">S'enregistrer</button>
+          </div>
+
+          <div class="form-input">
+
+            <p class="form-link">Déjà enregistrer ?
+              <router-link class="link" to="/login">Se connecter</router-link>
+            </p>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </section>
 
 </div>
 </template>
@@ -18,23 +52,22 @@ export default {
   name: 'register',
   data() {
     return {
-      name:'',
-      email:'',
-      password:'',
-      password_confirmation:''
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: ''
     }
   },
-    methods: {
-      register(){
-       console.log('ok enregistré');
-       firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then( user => {
-         console.log('utilisateur inscrit' + user.email);
+  methods: {
+    register() {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(user => {
+        console.log('utilisateur inscrit' + user.email);
 
-       }).catch( error => {
-         console.log(error);
-       })
-      }
+      }).catch(error => {
+        console.log(error);
+      })
     }
+  }
 
 }
 </script>
@@ -42,4 +75,74 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
+.img {
+  width: 100%;
+  text-align: center;
+}
+
+.logo {
+  width: 25%;
+  text-align: center;
+  margin: 12px;
+}
+
+.center_form {
+  max-width: 420px;
+  margin: 0 auto;
+  padding: 100px 20px 0 20px;
+}
+
+.form-input {
+  padding: 0 20px;
+  margin: 0 0 15px;
+}
+
+.form-style {
+  display: block;
+  width: 100%;
+  height: 46px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.4;
+  color: #555;
+  border: 0;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+.btn {
+  border-radius: 3px;
+  color: #fff;
+  font-size: 14px;
+  display: block;
+  border: none;
+  background: no-repeat;
+  background-image: linear-gradient(-180deg, #992623, #a84342 83%);
+  text-align: center;
+  text-decoration: none;
+  line-height: 54px;
+  width: 100%;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.btn:hover {
+  opacity: 0.8;
+}
+
+.form-link {
+  color: #d0d6db;
+}
+
+.link {
+  font-weight:500;
+  color: #a84342;
+  -webkit-transition: 0.5s;
+  /* Safari */
+  transition: 0.5s;
+}
+
+.link:hover {
+  color: #992623;
+}
 </style>
